a9d72d5e1edf38e3deab584397764877805e5d1e
christophercodinga11y <christopherjack14@gmail.com>
2025-04-07 14:48:20 -0500

    completed routing

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const router = express.Router()
 
 // static files
@@ -9,10 +10,18 @@ router.use('/jokes', require('./api/jokesRoutes'))
 // home page
 // localhost: 3000
 router.get('/', (req, res)=> {
-    res.render('pages/home', {
-        title: 'My Jokes Websites',
-        name: 'Jokes'
+    let jokeOfTheDay
+    
+    axios.get('https://api.sampleapis.com/jokes/goodJokes')
+    .then(resp => {
+        jokeOfTheDay = resp.data[Math.floor(Math.random() * resp.data.length)]
+        res.render('pages/home', {
+            title: 'My Jokes Websites',
+            name: 'Jokes',
+            joke: jokeOfTheDay
+        })
     })
+
 })
 
 
